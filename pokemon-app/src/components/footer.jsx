d4dd83e5b65eb4e10881c195635e328f1a77e2cf
andressaamaro495 <andressaamaro495@gmail.com>
2025-09-14 11:48:54 +0000

    Pastas crieadas: components, pages e services. Criação do cabeçalho e do rodapé

---
+++ pokemon-app/src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white p-4 text-center mt-4">
+        <p>Feito com ❤️ por Andressa, usando React e PokeAPI</p>
+      </footer>
+    );
+  }
